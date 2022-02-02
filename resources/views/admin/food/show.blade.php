ae62c2d9e3d4c31f4c94633d34ac17ab453e3800
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-02 19:26:52 +0100

    created_migration

---
+++ resources/views/admin/food/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+	{{-- <div class="container">
+		@if ($food)
+			<h2>
+				{{$food->product_name}}
+			</h2>
+
+				
+		
+		@else
+			
+		@endif
+	</div> --}}
+@endsection
