6d86d2793467000ccb3b74d16490c9374cb572e8
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-03 00:38:25 +0100

    when the table will be fixed I'll finish this view

--- resources/views/admin/food/show.blade.php
+++ resources/views/admin/food/show.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-	{{-- <div class="container">
+	<div class="container">
 		@if ($food)
 			<h2>
 				{{$food->product_name}}
 			</h2>
 
-				
-		
+			<div class="left">
+				<div class="thumbs"></div>
+				<div class="cover"></div>
+			</div>
+			<div class="right">
+				// brand
+				// price
+				// 
+			</div>
+	
 		@else
 			
 		@endif
-	</div> --}}
+	</div>
 @endsection
