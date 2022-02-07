3987f38c472f1dcac807ec0f5eb62d412306c451
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-07 13:02:57 +0100

    partially done show page

--- resources/views/admin/food/show.blade.php
+++ resources/views/admin/food/show.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
+	<div class="container food_show_page">
+		{{-- TODO - convenzione nomi classi del container per evitare conflitti nel css // creare file parziali per ogni singola pagina --}}
 		@if ($food)
 			<h2>
 				{{$food->product_name}}
 			</h2>
 
 			<div class="left">
-				<div class="thumbs"></div>
 				<div class="cover"></div>
+				<div class="thumbs 
+				@if(!isEmpty($arrayImmagini)) show @endif" {{-- TODO - convenzione nomi dati array --}}
+				>
+					@foreach ($arrayImmagini as $img)
+						<img src="{{$img->url}}" alt="{{$img->title}}" class="product_thumbnail">
+					@endforeach
+				</div>
 			</div>
 			<div class="right">
-				// brand
-				// price
-				// 
+				<p class="brand">{{$food->brand}}</p>
+				<p class="price">{{$food->price}}</p>
+				{{-- TODO - appena creiamo la tabella finisco la vista così ho una reference esatta dei dati a disposizione --}}
 			</div>
 	
 		@else
-			
+			<h2>No product found</h2>
 		@endif
 	</div>
 @endsection
