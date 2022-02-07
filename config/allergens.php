c0e19e3b292a3fea508da45e0e99a583df7244ff
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-07 16:34:35 +0100

    done allergens config file

---
+++ config/allergens.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+	'Glutine',
+	'Crostacei e derivati',
+	'Uova e derivati',
+	'Pesce e derivati',
+	'Arachidi e derivati',
+	'Soia e derivati',
+	'Latte e derivati',
+	'Frutta a guscio e derivati',
+	'Sedano e derivati',
+	'Senape e derivati',
+	'Semi di sesamo e derivati',
+	'Lupino e derivati',
+	'Molluschi e derivati',
+	'Anidride solforosa e solfiti',
+];
