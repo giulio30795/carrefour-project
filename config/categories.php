c20c6a2d4408df52129d11a0e82b68d7967cb5c3
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2022-02-09 17:19:50 +0100

    filling category table with seeder

---
+++ config/categories.php
@@ -0,0 +1,16 @@
+<?php 
+
+return [
+	'Pesce',
+	'Carne',
+	'Frutta',
+	'Verdura',
+	'Uova latte e latticini',
+	'Salumi e formaggi',
+	'Pane Farine e derivati',
+	'Frutta a guscio e derivati',
+	'Acqua e analcolici',
+	'Alcolici',
+	'Surgelati e Gelati',
+	'Prodotti Bio',
+];
