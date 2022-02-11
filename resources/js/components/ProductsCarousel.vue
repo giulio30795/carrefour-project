01144b0644af41308d0c923612ba3491b549eabd
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-11 14:54:35 +0100

    created temporary carousel component, working on product card

---
+++ resources/js/components/ProductsCarousel.vue
@@ -0,0 +1,17 @@
+<template>
+	<ProductCard />
+</template>
+
+<script>
+import ProductCard from './ProductCard.vue';
+export default {
+	name: 'Product_Carousel',
+	components: {
+		ProductCard,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
