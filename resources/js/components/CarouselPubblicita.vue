a900aadbb1e4594055d46c3d79ce71d4efd3d390
giulio30795 <giulioincarbone95@gmail.com>
2022-02-15 18:19:00 +0100

    check

--- resources/js/components/CarouselPubblicita.vue
+++ resources/js/components/CarouselPubblicita.vue
@@ -1,6 +1,24 @@
 <template>
-    <section>
-        test
+    <section class="container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                     <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwe593ac51/banner/main-carousel/food/banner_piccolo/scJohnson/Glade_PromoCard_LoveTheSign_WEB_993x275_B0222.jpg" class="d-block w-100" alt="img"> 
+                </div>
+                <div class="carousel-item">
+                    <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dw75186023/banner/main-carousel/food/banner_piccolo/danome-actimel/E_CRF_ACTIMEL_FEB_HP_Desktop_993x275px.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://www.carrefour.it/on/demandware.static/-/Sites/default/dwff7e212c/banner/main-carousel/food/banner_piccolo/risparmio-di-qualita/Bannerpiccolo_des_02.jpg" class="d-block w-100" alt="img">
+                </div>
+            </div>
+        </div>
+
     </section>
 </template>
 
