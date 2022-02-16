295cfeaec33de28e2b23fc2992c2923fe4a33e95
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-16 22:48:11 +0100

    refactoring navbar done

--- resources/js/partials/Navbar.vue
+++ resources/js/partials/Navbar.vue
@@ -20,13 +20,13 @@
             </form>
             
             <div class="right">
-                <div class="payback" >
+                <a class="payback" >
                     <img src="https://www.carrefour.it/on/demandware.static/Sites-carrefour-IT-Site/-/default/dw977fdad2/images/payback-ball.png" alt="">
-                    <a href="" class="pay text-center">
+                    <a href="" class="pay">
                         <div class="top">PAYBACK</div>
                         <div class="bot"><strong>Scopri Payback</strong></div>
                     </a>
-                </div>
+                </a>
 
                 <div>
                     <a href="">
@@ -39,7 +39,41 @@
     <!--UNDER-->
 
         <div class="down">
-            <div class="nav" >
+            <ul class="left">
+                <li>
+                    <a href="">Spesa Online</a>
+                </li>
+                <li>
+                    <a href="">Elettronica e Giocattoli</a>
+                </li>
+                <li>
+                    <a href="">Promozioni
+                        <i class="fa-solid fa-caret-down "></i>
+                    </a>
+                    
+                </li>
+            </ul>
+            <ul class="right">
+                <li>
+                    <a class="grey" href="">Spesa online</a>
+                </li>
+                <li>
+                    <a class="grey" href="">Elettronica e giocattoli</a>
+                </li>
+                <li>
+                    <a href="">Spesa Smart
+                        <i class="fa-solid fa-caret-down"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa-solid fa-user me-1"></i>
+                        <strong>Accedi</strong>
+                        <i class="fa-solid fa-caret-down ms-2"></i>
+                    </a>
+                </li>
+            </ul>
+            <!-- <div class="nav" >
                 <ul class="ps-0" >
                     <li>
                         <a href="" class="me-4">Spesa online</a>
@@ -67,7 +101,7 @@
                         <a href=""><i class="fa-solid fa-user me-1"></i><strong>Accedi</strong><i class="fa-solid fa-caret-down ms-2"></i></a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </div>
     </div>
@@ -103,7 +137,7 @@ export default {
     padding: .5rem 0;
     margin-bottom: 1rem;
     .form{
-        width: 40%;
+        width: 50%;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -113,7 +147,7 @@ export default {
             place-content: center;
             position: absolute;
             top: 50%;
-            right: .7rem;
+            right: .5rem;
             color: $clear-100;
             font-size: .8rem;
             transform: translateY(-50%);
@@ -121,6 +155,11 @@ export default {
             padding: .4rem;
             aspect-ratio: 1.0;
             border-radius: 50%;
+            cursor: pointer;
+            transition: filter .15s ease;
+            &:hover {
+                filter: brightness(80%);
+            }
         }
         input {
             width: 100%;
@@ -131,6 +170,10 @@ export default {
             &:focus {
                 outline: none;
             }
+            &::placeholder {
+                color: rgba($dark-900, .3);
+                font-size: .9rem;
+            }
         }
     }
 
@@ -138,9 +181,9 @@ export default {
         justify-content: flex-end;
         display: flex;
         align-items: center;
-        .cart{
-        margin: 0 30px;
-        font-size: 25px;
+        .cart {
+        margin: 0 2rem;
+        font-size: 1.5rem;
         }
     }
 }
@@ -149,49 +192,113 @@ export default {
     width: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     background-color: $clear-100;
-    ul{
+    .left,
+    .right {
+        width: 50%;
         display: flex;
-        list-style: none;
-             
-        a{
-            text-decoration: none;
-            color: #14386b;
-            &:hover { 
-                color: #3388ea;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+        li {
+            display: block;
+            a {
+                color: $primary-400;
+                text-decoration: none;
+                &:hover {
+                    color: $enabled;
                 }
+                &.active {
+                    color: $enabled;
+                }
+                i {
+                    margin-left: .4rem;
+                }
+            }
         }
-    } 
+    }
+    .left {
+        li {
+            margin-right: 2.2rem;
+        }
+    }
+    .right {
+        justify-content: flex-end;
+        font-size: .75rem;
+        li {
+            margin-left: 1rem;
+            &::after {
+                content: "|";
+                margin-left: 1rem;
+                color: rgba($disabled-text, .2);
+            }
+            &:nth-child(3),
+            &:nth-child(4) {
+                font-size: 1rem;
+            }
+            a.grey {
+                color: $disabled-text;
+                &:hover {
+                    color: $enabled;
+                }
+            }
+        }
+    }
+    // width: 100%;
+    // display: flex;
+    // justify-content: space-between;
+    // align-items: center;
+    // background-color: $clear-100;
+    // ul{
+    //     display: flex;
+    //     list-style: none;
+             
+    //     a{
+    //         text-decoration: none;
+    //         color: #14386b;
+    //         &:hover { 
+    //             color: #3388ea;
+    //             }
+    //     }
+    // } 
 
 }
 
 .payback{
+    display: flex;
+    flex-direction: column;
     position: relative;
-    width: 165px;
-    height: 38px;
-    background-color: transparent;
-    border-radius: 8px;
-    border: 2px solid #d0e4f5;
+    border-radius: .5rem;
+    border: 2px solid rgba($primary-100, .15);
+    cursor: pointer;
+    padding-inline: 1.5rem 1rem;
     &:hover {
-        background-color: #d0e4f5;
+        background-color: rgba($primary-100, .15);
+        border: 2px solid transparent;
     }
 
-    img{
+    img {
         position: absolute;
         left: -20px;
-        width: 37px;
+        width: 2rem;
     }
 
-    .pay{
+    .pay {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
         text-decoration: none;
-
-        .top{
-            font-size: 10px;
-            color: #14386b;
+        .top {
+            font-size: .57rem;
+            color: $primary-400;
         }
-        .bot{
-            font-size: 17px;
-        } 
+        .bot {
+            font-size: 1rem;
+            strong {
+                text-decoration: none;
+            }
+        }
     }
 }
 
